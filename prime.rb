d31922f288d827e8f2c332a886df39ca706691c9
Melissa Real <melissa.real@hotmail.com>
2018-04-26 19:26:12 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,18 @@
 def prime?(int)
-  collection = [1..100]
+  collection =(1..100).to_a
   array_of_ints = []  
   
   collection.each do |nr|
-    if int % nr != 0
+    
+    if int % nr == 0
      array_of_ints.push(int % nr)  
     end 
+    
   end 
-  return array_of_ints
+  if array_of_ints.length > 2 
+    return false 
+  else 
+    return true 
+  end 
     
 end
